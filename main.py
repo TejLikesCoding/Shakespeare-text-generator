891bcfc53cca55a44c5c57ae997d45e468e57e4a
TejLikesCoding <129396191+TejLikesCoding@users.noreply.github.com>
2024-09-19 16:04:02 +0100

    Update main.py
    Code cleansiness

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ index_to_char = dict((i, c) for i, c in enumerate(characters))
 SEQ_length = 40
 STEP_SIZE = 3
 
+#Below is the code used to train my model
 '''
 sentences = []
 next_characters = []
@@ -54,15 +55,6 @@ model = tf.keras.models.load_model('textgenerator.model1')
 
 
 
-def sample(preds, temperature=1.0):
-    preds = np.asarray(preds).astype('float64')
-    preds = np.log(preds)/temperature
-    exp_preds = np.exp(preds)
-    preds = exp_preds / np.sum(exp_preds)
-    probas = np.random.multinomial(1, preds, 1)
-    return np.argmax(probas)
-
-#takes predictions and picks one character depending on temperature, High = risky, Low = Safe
 
 def sample(preds, temperature=1.0):
     preds = np.asarray(preds).astype('float64')
@@ -99,32 +91,3 @@ generated_text = generate_text(length=100, temperature=0.5)
 print(generated_text)
 
 
-'''
-def generate_text(length, temperature):
-    # Start with a random seed sequence from your text
-    start_index = random.randint(0, len(text) - SEQ_length - 1)
-    seed_text = text[start_index: start_index + SEQ_length]
-
-    generated = seed_text
-    sentence = seed_text
-
-    for i in range(length):
-        x = np.zeros((1, SEQ_length, len(characters)))
-
-        for t, character in enumerate(sentence):
-            x[0, t, char_to_index[character]] = 1
-
-        predictions = model.predict(x, verbose=0)[0]
-        next_index = sample(predictions, temperature)
-        next_character = index_to_char[next_index]
-
-        generated += next_character
-        sentence = sentence[1:] + next_character
-
-    return generated
-
-# Usage example:
-generated_text = generate_text(length=200, temperature=0.6)
-print(generated_text)
-
-'''
